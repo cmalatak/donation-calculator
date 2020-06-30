f7380de0d21b43de2399e2a2b436df775a0ed610
Cassie Malatak <cjmalatak@gmail.com>
2020-06-29 21:18:40 -0400

    - create app - add income input - add at a glance result modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [income, setIncome] = useState("Income");
+  const [modalClass, setModalClass] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>How much should I be donating?</h1>
+      <div className="at-a-glance-calc">
+        <h3>At a glance:</h3>
+        <div className="arguments">
+          <label htmlFor="income">
+            Enter your annual income:
+            <input
+              type="number"
+              id="income"
+              value={income}
+              placeholder={income}
+              onChange={(event) => setIncome(event.target.value)}
+              onBlur={(event) => setIncome(event.target.value)}
+            ></input>
+          </label>
+          <button
+            className="calc-at-a-glance-result"
+            onClick={() => setModalClass("show-modal")}
+          >
+            Submit
+          </button>
+          <div className={modalClass === "" ? "modal" : modalClass}>
+            <div className="modal-content">
+              <span
+                className="close-button"
+                onClick={() => setModalClass("modal")}
+              >
+                &times;
+              </span>
+              <h3>You can donate ${} a month,</h3>
+              <h4>and still have ${} left in free spending.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
